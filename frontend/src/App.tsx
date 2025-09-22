451082edd4bcb1145fe8993f0413388d92093ce3
Hubert Leo <hubertleo@Huberts-MacBook-Air.local>
2025-09-22 11:24:08 -0400

    initial frontend

---
+++ frontend/src/App.tsx
@@ -0,0 +1,24 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      | X | X | X |
+      <br></br>
+      -------------
+      <br></br>
+      | X | X | X | 
+      <br></br>
+      -------------
+      <br></br>
+      | 0 | 0 | 0 |
+    </>
+  )
+}
+
+export default App
